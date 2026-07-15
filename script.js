214fe38c05b4a936259e42ed12f2f411f5c34b1f
tegrblx-beep <tegrblx@gmail.com>
2026-07-15 15:08:22 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -663,6 +663,18 @@ var BADGERS = [
     badges: [
     ]
   },
+  {
+    id: "badge-island-challenges",
+    name: "Badge Island Challenges",
+    difficulty: "Hard",
+    gameLink: "https://www.roblox.com/games/13666913701/Badge-Island-Challenges",
+    milestones: [
+      { name: "Ascended Collector:", target: 10 },
+    ],
+    sheetUrl: "https://docs.google.com/spreadsheets/d/1_GXidotsL0OtM2J2FJ1f-cGVgo_E1LR4I_L0xm4Bz1M/edit?gid=845663609#gid=845663609",
+    badges: [
+    ]
+  },
   // Add more badger objects here, separated by commas.
 ];
 // ==================================================================================
